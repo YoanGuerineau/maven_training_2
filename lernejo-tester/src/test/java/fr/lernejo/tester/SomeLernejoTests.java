996468be5d9e134914deac6e0765b169257192c8
E168339C <yoan.guerineau@etu.univ-nantes.fr>
2021-12-01 15:34:26 +0100

    Part 3 Add API

---
+++ lernejo-tester/src/test/java/fr/lernejo/tester/SomeLernejoTests.java
@@ -0,0 +1,17 @@
+package fr.lernejo.tester;
+
+import fr.lernejo.tester.api.TestMethod;
+
+public class SomeLernejoTests {
+    @TestMethod
+    public void ok() {
+
+    }
+    public void ko() {
+        throw new IllegalStateException();
+    }
+    @TestMethod
+    public void none() {
+
+    }
+}
